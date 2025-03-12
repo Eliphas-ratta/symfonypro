e99fcbf7226dcb53406e4161161a8b45c93cc36c
06011-39-07 <06011-39-07@exchange.ad.afpanet>
2025-03-12 16:22:07 +0100

    regitration and ogin done almost

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,7 @@ import './bootstrap.js';
  */
 import './styles/app.css';
 import './styles/app.scss';
+import './bootstrap.js';
 
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -3,3 +3,8 @@ import { startStimulusApp } from '@symfony/stimulus-bundle';
 const app = startStimulusApp();
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
+// Importer les styles de Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Importer les scripts de Bootstrap
+import 'bootstrap';
