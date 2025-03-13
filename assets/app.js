3415bf98b1f3ebdb711905f2aa2d2cd892e5800d
06011-39-07 <06011-39-07@exchange.ad.afpanet>
2025-03-13 16:25:37 +0100

    header  done

--- assets/app.js
+++ assets/app.js
@@ -5,9 +5,9 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+import './bootstrap.js';
 import './styles/app.css';
 import './styles/app.scss';
-import './bootstrap.js';
 
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ProfileController extends AbstractController
+{
+    #[Route('/profile', name: 'app_profile')]
+    public function index(): Response
+    {
+        // Récupérer les informations de l'utilisateur connecté
+        $user = $this->getUser();
+
+        if (!$user) {
+            // Redirige vers la page de connexion si l'utilisateur n'est pas connecté
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('profile/index.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
